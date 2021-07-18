6632e62537138a5d7833a28a67f77b35a8042a9f
Vladyslav Cherniuk <vlad26v03@gmail.com>
2021-07-18 20:16:58 +0300

    Added extract function in layouts

--- resources/views/cart.php
+++ resources/views/cart.php
@@ -2,13 +2,14 @@
     <?php
     ob_start();
     foreach ($data as $product) {
+        extract($product);
         ?>
         <div class="cart-product">
             <div class="d-flex">
-                <img class="cart-img" src="../../public/img/<?= $product["photo"] ?>" alt="Computer">
-                <a href="/?page=product&id=<?= $product["id"] ?>"><?= $product["name"] ?></a>
+                <img class="cart-img" src="../../public/img/<?= $photo ?>" alt="Computer">
+                <a href="/?page=product&id=<?= $id ?>"><?= $name ?></a>
             </div>
-            <p class="cart-product-price"><?= $product["price"] ?><span class="hryvnia-sign">₴</span></p>
+            <p class="cart-product-price"><?= $price ?><span class="hryvnia-sign">₴</span></p>
         </div>
         <?php
     }
