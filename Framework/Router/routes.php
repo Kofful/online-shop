0ec8ce43eb2b3f8c6904e303f6d13e9b2fb27014
Vladyslav Cherniuk <vlad26v03@gmail.com>
2021-08-16 12:00:35 +0300

    Added auth middleware to cart route

--- Framework/Router/routes.php
+++ Framework/Router/routes.php
@@ -3,7 +3,7 @@ use Framework\Router\Router;
 
 Router::get("/", "HomeController");
 Router::get("/catalog", "CatalogController");
-Router::get("/cart", "CartController");
+Router::get("/cart", "CartController", ["name"=>"auth", "params"=>["statement"=>true]]);
 Router::get("/product/([0-9]+)", "ProductController");
 Router::get("/login", "LoginController", ["name"=>"auth","params"=>["statement"=>false]]);
 Router::get("/register", "RegisterController", ["name"=>"auth","params"=>["statement"=>false]]);
