5d47a649ca26560f4f11197baec8d66cb1dd10f5
Vladyslav Cherniuk <vlad26v03@gmail.com>
2021-08-17 11:18:06 +0300

    Added message about empty cart

--- App/View/Templates/cart.php
+++ App/View/Templates/cart.php
@@ -1,18 +1,26 @@
 <div class="cart-container">
     <?php
     ob_start();
-    foreach ($data as $product) {
+    if (sizeof($data) === 0) :
         ?>
-        <div class="cart-product">
-            <div class="d-flex">
-                <img class="cart-img" src="../../public/img/<?= $product->getPhoto() ?>" alt="Computer">
-                <a href="/product/<?= $product->getId() ?>"><?= $product->getName() ?></a>
-            </div>
-            <p class="cart-product-price"><?= $product->getPrice() ?><span class="hryvnia-sign">₴</span></p>
+        <div class="no-products">
+            Корзина пуста
         </div>
         <?php
-    }
-    ob_end_flush() ?>
+    else :
+        foreach ($data as $product) {
+            ?>
+            <div class="cart-product">
+                <div class="d-flex">
+                    <img class="cart-img" src="../../public/img/<?= $product->getPhoto() ?>" alt="Computer">
+                    <a href="/product/<?= $product->getId() ?>"><?= $product->getName() ?></a>
+                </div>
+                <p class="cart-product-price"><?= $product->getPrice() ?><span class="hryvnia-sign">₴</span></p>
+            </div>
+            <?php
+        }
+        ob_end_flush() ?>
 
     <button class="btn btn-success btn-order">Заказать</button>
+    <?php endif;?>
 </div>
