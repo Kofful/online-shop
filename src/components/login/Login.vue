db1facf874ddc37b65cd20a796e57ab812dd34c8
Vladyslav Cherniuk <vlad26v03@gmail.com>
2021-08-25 21:30:27 +0300

    Added validation at login and register page

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -2,20 +2,43 @@
   <form class="login-form" method="post">
     <div class="input-data">
       <label for="phone-input">Номер телефона</label>
-      <input id="phone-input" name="phone" class="login-input form-control" style="
-  -webkit-appearance: none;" type="number" autocomplete="off">
+      <input id="phone-input" name="phone"
+             :class="{'login-input form-control': true, 'is-invalid': !isPhoneValid}"
+             type="text" autocomplete="off" v-model="phone">
       <label for="password-input">Пароль</label>
-      <input id="password-input" name="password" class="login-input form-control" type="password">
+      <input id="password-input" name="password"
+             :class="{'login-input form-control': true, 'is-invalid': !isPasswordValid}"
+             type="password" v-model="password">
     </div>
     <div class="link-div">
       <p>Еще нет аккаунта? <a href="/register">Зарегистрируйтесь</a></p>
-      <button class="btn btn-success">Войти</button>
+      <button class="btn btn-success" :disabled="!isFormValid">Войти</button>
     </div>
   </form>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data: () => ({
+    phone: "",
+    password: "",
+    isPhoneValid: true,
+    isPasswordValid: true
+  }),
+  watch: {
+    phone(newValue) {
+      this.isPhoneValid = newValue.match(/^\+?[0-9]{10,13}$/);
+    },
+    password(newValue) {
+      this.isPasswordValid = newValue.length >= 4;
+    }
+  },
+  computed: {
+    isFormValid() {
+      return (this.isPhoneValid && !!this.phone.length &&
+          this.isPasswordValid && !!this.password.length);
+    }
+  }
 }
 </script>
--- src/components/register/Register.vue
+++ src/components/register/Register.vue
@@ -2,20 +2,43 @@
   <form class="login-form" method="post">
     <div class="input-data">
       <label for="phone-input">Номер телефона</label>
-      <input id="phone-input" name="phone" class="login-input form-control" style="
-  -webkit-appearance: none;" type="number" autocomplete="off">
+      <input id="phone-input" name="phone"
+             :class="{'login-input form-control': true, 'is-invalid': !isPhoneValid}"
+             type="text" autocomplete="off" v-model="phone">
       <label for="password-input">Пароль</label>
-      <input id="password-input" name="password" class="login-input form-control" type="password">
+      <input id="password-input" name="password"
+             :class="{'login-input form-control': true, 'is-invalid': !isPasswordValid}"
+             type="password" v-model="password">
     </div>
     <div class="link-div">
       <p>Уже есть аккаунт? <a href="/login">Вход</a></p>
-      <button class="btn btn-success">Зарегистрироваться</button>
+      <button class="btn btn-success" :disabled="!isFormValid">Зарегистрироваться</button>
     </div>
   </form>
 </template>
 
 <script>
 export default {
-  name: "Register"
+  name: "Register",
+  data: () => ({
+    phone: "",
+    password: "",
+    isPhoneValid: true,
+    isPasswordValid: true
+  }),
+  watch: {
+    phone(newValue) {
+      this.isPhoneValid = newValue.match(/^\+?[0-9]{10,13}$/);
+    },
+    password(newValue) {
+      this.isPasswordValid = newValue.length >= 4;
+    }
+  },
+  computed: {
+    isFormValid() {
+      return (this.isPhoneValid && !!this.phone.length &&
+          this.isPasswordValid && !!this.password.length);
+    }
+  }
 }
 </script>
